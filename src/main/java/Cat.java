9600aa20019d829296337ba45a50f8e55833df5e
unknown <lwrow@rambler.ru>
2025-03-04 16:22:17 +0300

    modified App, AppConfig, Cat, AppTest

--- src/main/java/Cat.java
+++ src/main/java/Cat.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
+import org.springframework.context.annotation.Scope;
 
+// д.б. POJO класс Cat
+//    @Scope - новый бин при каждом запросе
 public class Cat {
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
